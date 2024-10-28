6d36d09a53728f596e75b56aefce4b6e5e0e89a5
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-10-27 21:46:43 -0400

    style header

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -15,7 +15,7 @@ const NavLink = ({
   const isActive = pathname === href;
 
   return (
-    <li className="mx-4">
+    <li className="ml-4">
       <Link href={href} legacyBehavior>
         <a
           className={`dark:text-slate-300 text-lg text-gray-800 ${
@@ -32,11 +32,15 @@ const NavLink = ({
 const Navbar = () => {
   return (
     <nav className="dark:bg-gray-800 bg-slate-300 p-2 border-b dark:border-slate-300 border-gray-800">
-      <ul className="flex justify-around list-none m-0 p-0">
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/blog">Blog</NavLink>
-        <NavLink href="/about">About</NavLink>
-        <NavLink href="/projects">Projects</NavLink>
+      <ul className="flex justify-between list-none m-0 p-0">
+        <Link href="/" className={`dark:text-slate-300 text-lg text-gray-800`}>
+          Peter Walker
+        </Link>
+        <div className="flex ">
+          <NavLink href="/blog">Blog</NavLink>
+          <NavLink href="/about">About</NavLink>
+          <NavLink href="/projects">Projects</NavLink>
+        </div>
       </ul>
     </nav>
   );
